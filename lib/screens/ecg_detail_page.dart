7ed38f73683e483cc2a32d6b08623c954915d7d7
yunkyung318 <dbsrud318@naver.com>
2025-10-12 16:25:15 +0900

    2025.10.12_reconstruction_qt_st_update_v2

--- lib/screens/ecg_detail_page.dart
+++ lib/screens/ecg_detail_page.dart
@@ -94,7 +94,7 @@ class _EcgDetailPageState extends State<EcgDetailPage> {
         if (response.statusCode == 200) {
           debugPrint("Response: ${response.body}");
           final jsonData = jsonDecode(response.body);
-          final resultArray = jsonData['result'];
+          /**final resultArray = jsonData['result'];
           final leads = resultArray[0][0].sublist(3, 14);
           for (int i = 0; i < 11; i++) {
             leadData[i + 1] = List.generate(
@@ -116,6 +116,29 @@ class _EcgDetailPageState extends State<EcgDetailPage> {
 
             debugPrint('QT Intervals: $qtIntervals');
             debugPrint('ST Segments: $stSegments');
+          }**/
+          final resultArray = jsonData['result'] as List<dynamic>;
+          final allLeads = resultArray[0] as List<dynamic>;
+
+          for (int i = 0; i < allLeads.length; i++) {
+            final List<dynamic> lead = allLeads[i];
+            leadData[i] = List.generate(
+              lead.length,
+                  (j) => FlSpot(j * (10.0 / 512.0), (lead[j] as num).toDouble()),
+            );
+          }
+          
+          final qtStData = jsonData['qt_st'];
+          if (qtStData != null) {
+            qtIntervals = (qtStData['qt_intervals'] as List<dynamic>)
+                .where((value) => value is num && !value.isNaN)
+                .map((e) => (e as num).toDouble())
+                .toList();
+
+            stSegments = (qtStData['st_segments'] as List<dynamic>)
+                .where((value) => value is num && !value.isNaN)
+                .map((e) => (e as num).toDouble())
+                .toList();
           }
         } else {
           debugPrint('❌ 서버 오류: ${response.reasonPhrase}');
